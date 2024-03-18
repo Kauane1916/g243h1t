86297e9fe4ba930c08559c892c4d2da145d8ad7b
Kauane1916 <126519169+Kauane1916@users.noreply.github.com>
2024-03-18 12:28:05 -0300

    Add files via upload

--- js script.js
+++ js script.js
@@ -1,32 +1,50 @@
 imprime = (msg) => console.log(msg);
+const data = new date ();
+function hora_atual(){
+  setTimeout( () => {
+    document.getElementById("h").innerHTML = data.getHours 
+    "!" + data.getSeconds();
+    hora_atual();
+  }, 1000);
+  document.getElementById("h").innerHTML =
+  data.getHours() + ":" + data.getSeconds();
+}
 
-function escolher_aba(bnt){
+fazerSorteio = () => {
+  num1 = document.getElementById("inicio").value ;
+  num2 =  document.getElementById("fim").value ;
+  let sorteio = Math.floor(Math.random() * Number(num2) - Number(num1));
+
+  document.getElementById("resultado").innerHTML = sorteio;
+}
+function escolher_aba(btn){
     for(let i = 1; i <= 4; i++){
         let id_btn = "btn"+i;
         let class_aba = "aba"+i;
-        document.getElementById("btn1").style.backgroundColor = "#ffffff" 
-        document.getElementById("bnt1").diabled =true;
-    }
+        document.getElementById(id_btn).style.backgroundColor = "#ffffff";
+        document.getElementById(id_btn).disabled = true;
+        document.getElementById(class_aba).style.display = "none";
+    
     if(btn == "1"){
-      document.getElementById(id_btn).style.backgroundColor = "#ffffff" 
-      document.getElementById(id_btn).diabled =false;
-      document.getElementById(id_btn).style.display = "blok";
+      document.getElementById("btn1").style.backgroundColor = "#ffffff" ;
+      document.getElementById("btn1").disabled =true;
+      document.getElementById("aba1").style.display = "block";
     }
     if(btn == "2"){
-        document.getElementById(id_btn).style.backgroundColor = "#ffffff" 
-      document.getElementById(id_btn).diabled =false;
-      document.getElementById(id_btn).style.display = "blok";
+        document.getElementById("btn2").style.backgroundColor = "#ffffff" ;
+        document.getElementById("btn2").disabled =true;
+      document.getElementById("aba2").style.display = "block";
     }
     if(btn == "3"){
-        document.getElementById(id_btn).style.backgroundColor = "#ffffff" 
-        document.getElementById(id_btn).diabled =false;
-        document.getElementById(id_btn).style.display = "blok";
+        document.getElementById("btn3").style.backgroundColor = "#ffffff" ;
+        document.getElementById("btn3").disabled =true;
+        document.getElementById("aba3").style.display = "block";
       }
   
      if(btn == "4"){
-        document.getElementById(id_btn).style.backgroundColor = "#ffffff" 
-        document.getElementById(id_btn).diabled =false;
-        document.getElementById(id_btn).style.display = "blok";
+        document.getElementById("btn4").style.backgroundColor = "#ffffff" ;
+        document.getElementById("btn4").disabled =true;
+        document.getElementById("aba4").style.display = "block";
       }
-  
+    }
 }
