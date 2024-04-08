41e969d39b6d027ced9ff8613e036be484dc1112
Kauane1916 <126519169+Kauane1916@users.noreply.github.com>
2024-04-08 12:25:38 -0300

    Add files via upload

--- js script.js
+++ js script.js
@@ -1,5 +1,5 @@
 imprime = (msg) => console.log(msg);
-const data = new date ();
+const data = new Date ();
 function hora_atual(){
   setTimeout( () => {
     document.getElementById("h").innerHTML = data.getHours 
@@ -18,13 +18,14 @@ fazerSorteio = () => {
   document.getElementById("resultado").innerHTML = sorteio;
 }
 function escolher_aba(btn){
+
     for(let i = 1; i <= 4; i++){
         let id_btn = "btn"+i;
         let class_aba = "aba"+i;
         document.getElementById(id_btn).style.backgroundColor = "#ffffff";
-        document.getElementById(id_btn).disabled = true;
+        document.getElementById(id_btn).disabled = false;
         document.getElementById(class_aba).style.display = "none";
-    
+    }
     if(btn == "1"){
       document.getElementById("btn1").style.backgroundColor = "#ffffff" ;
       document.getElementById("btn1").disabled =true;
@@ -39,6 +40,10 @@ function escolher_aba(btn){
         document.getElementById("btn3").style.backgroundColor = "#ffffff" ;
         document.getElementById("btn3").disabled =true;
         document.getElementById("aba3").style.display = "block";
+        iniciar_despertador();
+        document.getElementById("hora_d").value = hora_d;
+        document.getElementById("min_d").value = min_d;
+        document.getElementById("seg_d").value = seg_d;
       }
   
      if(btn == "4"){
@@ -46,7 +51,7 @@ function escolher_aba(btn){
         document.getElementById("btn4").disabled =true;
         document.getElementById("aba4").style.display = "block";
       }
-    }
+    
 }
 
 let contador = null;
@@ -60,14 +65,33 @@ function contagem(){
   }
 }
 
-
-
-
-
 function iniciar(){
     inicio = document.getElementById("inicio").value;
     fim = document.getElementById("fim").value;
     contador = setIterval(() => {
       contagem();
     }, 1000);
+}
+let hora_d;
+let min_d;
+let seg_d;
+function iniciar_despertador(){
+  let d = new Date();
+  hora_d = d.getHours();
+  min_d = d.getMinutes();
+  seg_d = d.getSeconds();
+}
+let despertar =null;
+function ligar_despertador(){
+  let d = new Date();
+  if(d.getHours() == hora_d && d.getMinutes() == min_d && d.getSeconds() == seg_d){clearInterval(despertar);
+    alert("Alarme.");
+    escolher_aba(3);
+  }
+}
+function ajustar(){
+  document.getElementById("hora_d").value;
+  document.getElementById("min_d").value;
+  document.getElementById("seg_d").value;
+  despertar = setInterval(ligar_despertador,1000);
 }
